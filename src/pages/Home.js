248d9cdf44b70ec8669212650975476c37dc9e1e
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-03-17 17:24:48 -0400

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,55 @@
 import supabase from "../config/supabaseClient"
 import { useEffect, useState } from "react"
 
+import SmoothieCard from "../components/SmoothieCard";
+
 const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [smoothies, setSmoothies] = useState(null);
 
 
   useEffect(() => {
-    const fetchSmoothies = async() =>{
-      const {data, error} = await supabase
-      .from('smoothies') //name on tableonsupabase
-      .select() //grabs all items in specified table when blank
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from('smoothies') //name on tableonsupabase
+        .select() //grabs all items in specified table when blank
 
-      if(error){
-        setFetchError('Could not fetch the smoothies');
+      if (error) {
+        setFetchError('Could not fetch the smoothies')
         setSmoothies(null)
         console.log(error)
-      
+
       }
 
-      if(data){
-        setSmoothies(data);
+      if (data) {
+        setSmoothies(data)
         setFetchError(null)
       }
 
-      
+
     }
     fetchSmmothies() //function call
-          
+
   }, [])
 
-  
+
   return (
     <div className="page home">
-    {fetchError && (<p>{fetchError}</p>)}
-    {smoothies && ( <div>
-      {smoothies.map(smoothie =>(
-        <p>{smoothie.title}</p>
-      ))}
-      </div> )}
-      
+      {fetchError && (<p>{fetchError}</p>)}
+      {smoothies && (
+        <div className="smoothies">
+        {/* order using buttons */}
+        <div className="smoothie-grid">
+          {smoothies.map((smoothie) => (
+            
+            <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+          ))}
+        </div>
+        </div>
+        )}
+
     </div>
-  )
+  );
 }
 
 export default Home

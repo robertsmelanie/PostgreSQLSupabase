4c919dda571e0ffec1e1989c7cd36ff2b2036608
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-03-20 09:20:46 -0400

    update

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,11 +1,35 @@
-import React from 'react'
+// import React from 'react'
+import { Link } from "react-router-dom"
+import supabase from "../config/supabaseClient"
 
 const SmoothieCard = ({ smoothie }) => {
+
+    const handleDelete = async () =>{
+        const {data, error} = await supabase
+        .from('smoothies')
+        .delete()
+        .eq('id', smoothie.id)
+        if(error){
+            console.log(error)
+        }
+        if(data){
+            console.log(data)
+        }
+    }
+
+
+
     return (
         <div className='smoothie-card'>
             <h3>{smoothie.title}</h3>
             <p>{smoothie.method}</p>
             <div className='rating'>{smoothie.rating}</div>
+            <div className="butttons">
+                <Link to={'/' + smoothie.id}>
+                    <i className="material-icons">edit</i>
+                </Link>
+                <i className="material-icons" onClick={handleDelete}>delete</i>
+            </div>
         </div>
     )
 }
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,52 +1,80 @@
 import { useState } from "react"
-// import supabase from "../config/supabaseClient"
-// import {useNavigate} from "react-router-dom"
+import supabase from '../config/supabaseClient'
+import {useNavigate} from 'react-router-dom'
 
 const Create = () =>{
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [rating, setRating] = useState('')
+  
   const [formError, setFormError] = useState(null)
 
-const handleSubmit = async (e) =>{
-  e.preventDefault();
+  
 
-  if(!title || !method || !rating){
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
 
-    setFormError('Please fill in all the fields');
+    if(!title || !method || !rating ){
+
+      setFormError('Please fill in all the fields');
       return
     }
+    console.log(title,rating,method)
+    const {data, error} = await supabase
+      .from('smoothies')
+      .insert([{title, method, rating}])
 
-    console.log(title, rating, method)
-}
+        if(error){
+          console.log(error)
+          setFormError('Please fill in all fields');
 
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="title">Title:</label>
+        }
+     
+      
+          setFormError(null)
+          navigate("/")
+     
+
+    
+  }
+  return (
+    <div className="page create">
+      <h2>Create</h2>
+       
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
         <input 
         type="text"
         id="title"
         value={title}
         onChange={(e) => setTitle(e.target.value)} />
 
-      <label htmlFor="method">Method:</label>
+       
+
+
+        <label htmlFor="method">Method:</label>
+        {/* <time dateTime=""></time> */}
         <textarea
         type="text"
         id="method"
         value={method}
         onChange={(e) => setMethod(e.target.value)} />
 
-      <label htmlFor="rating">Rating:</label>
-       <input 
-       type="text"
-       id="rating"
-       value={rating}
-       onChange={(e) => setRating(e.target.value)} />
-       <button>Create Smoothie Recipe</button>
+        <label htmlFor="rating">Rating:</label>
+        <input 
+        type="text"
+        id="rating"
+        value={rating}
+        onChange={(e) => setRating(e.target.value)} />
+        <button>Create Smoothie Recipe</button>
 
         {formError && <p className="error">{formError}</p>}
-    </form>
-  </div>
+      </form>
+    </div>
+  )
+}
+
   
 
 
